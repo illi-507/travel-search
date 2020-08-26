291202d59b4fb084c4a34bddcbc62341cd681bf3
XINGJIAN WANG <wangxingjian94@gmail.com>
2020-08-25 22:15:24 -0400

    ready for deploy

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 // Define the routes.
 app.use('/api/places', require('./routes/api/places'));
 app.use('/api/yelp', require('./routes/api/yelp'));
 
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
